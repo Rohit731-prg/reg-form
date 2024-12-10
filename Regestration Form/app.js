6ef3fba9bb7394d347ab6273b41a94c2cf22fb60
Rohit731-prg <rohitsingha821380@gmail.com>
2024-12-10 21:40:12 +0530

    Updated Form App

---
+++ Regestration Form/app.js
@@ -0,0 +1,8 @@
+document.querySelector('.btn').addEventListener('click', () => {
+    const name = document.getElementById('name').value;
+    if(name === '') {
+        if(name.length < 15) {
+            
+        }
+    }
+})
